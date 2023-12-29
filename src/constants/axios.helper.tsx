e3969b3347bcece339496d172cd9e48f40235a2b
manish gandotra <manish.gandotra@icloud.com>
2023-12-29 22:55:29 +0530

    changes

--- src/constants/axios.helper.tsx
+++ src/constants/axios.helper.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse, AxiosError } from 'axios';
 import resp from "./error.helper";
-const API = 'http://localhost:9000/api'
+const API = 'https://infinite-ocean-11002-5e980ae4a0c9.herokuapp.com/api'
 
 function _helperaxios(url: string, params: any, method: string) {
     return axios({
